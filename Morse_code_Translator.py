d1ed557c256fc60a55be3a657e619e834cf062b9
WillPhillipsCVdemo <willphillips1984@gmail.com>
2018-05-15 22:41:56 +0100

    Update Morse_code_Translator.py

--- Morse_code_Translator.py
+++ Morse_code_Translator.py
@@ -22,13 +22,31 @@ CODE = {'A': '.-',   'B': '-...',  'C': '-.-.',
 path = "Output"
 os.makedirs(path, exist_ok=True)
 
+
+
+
 #Ask for user input
 message = input("Please enter a message to translate: ").upper()
 
 ##Create out test file for message
-with open("Output/Message.txt", "w") as file:
+with open("Output/Message.txt", "a") as file:
     file.write(message)
 
+'''
+
+###Read Email
+print("The code will now translate the email....")
+print("\n")
+###Reads input file
+with open("Output/Email_Message.txt", "r") as file:
+    message=file.read()
+    print(message.upper())
+
+
+##Create out test file for message
+with open("Output/Message.txt", "a") as file:
+    file.write(message.upper())
+'''
 
 #List
 l =[]
@@ -41,14 +59,14 @@ def translate(message):
     for word in words:
         ##For each character
         for char in word:
-            print(CODE[char])
+            #print(CODE[char])
             l.append(CODE[char])
             #l.append("/")
-        l.append("/")
-        print("\n")
+        l.append("\n")
+        #print("\n")
 
 ##Call method
-translate(message)
+translate(message.upper())
 
 ##Join list into a string
 morsecode = " ".join(l)
